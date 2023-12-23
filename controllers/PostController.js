1567132498dbe04c57a2581693613f9afe8db4f7
Albert Taygibov <albert.ti.2017@yandex.ru>
2023-12-24 02:01:17 +0300

    feat: finished creating server requests

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -14,7 +14,34 @@ export const getAll = async (req, res) => {
   }
 }
 
-export const getById = async (req, res) => {}
+export const getById = async (req, res) => {
+  try {
+    const postId = await PostModel.findOneAndUpdate(
+      {
+        _id: req.params.id, // элемент
+      },
+      {
+        $inc: { viewCount: 1 }, // обновить
+      },
+      {
+        returnDocument: 'after', // обновить и вернуть обновленный результат после обнов...
+      },
+    )
+
+    if (!postId) {
+      return res.status(404).json({
+        message: 'Статья не найдена',
+      })
+    }
+
+    res.json(postId)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    })
+  }
+}
 
 export const create = async (req, res) => {
   try {
@@ -38,6 +65,54 @@ export const create = async (req, res) => {
     console.log(error)
     res.status(500).json({
       message: 'Не удалось создать статью',
+    })
+  }
+}
+
+export const remove = async (req, res) => {
+  try {
+    const deletedPost = await PostModel.findByIdAndDelete({
+      _id: req.params.id,
+    })
+
+    if (!deletedPost) {
+      return res.status(404).json({
+        message: 'Статья не найдена',
+      })
+    }
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Не удалось удалить статью',
+    })
+  }
+}
+
+export const update = async (req, res) => {
+  try {
+    await PostModel.updateOne(
+      {
+        _id: req.params.id,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      },
+    )
+
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Не удалось обновить статью',
     })
   }
 }
--- index.js
+++ index.js
@@ -23,13 +23,13 @@ app.post('/auth/register', registerValidation, userController.register)
 app.post('/auth/login', loginValidation, userController.login)
 app.get('/auth/me', checkAuth, userController.getMe)
 
+app.post('/posts', checkAuth, postCreateValidation, postController.create)
 app.get('/posts', postController.getAll)
 app.get('/posts/:id', postController.getById)
-app.post('/posts', checkAuth, postCreateValidation, postController.create)
-// app.patch('/posts', postController.update)
-// app.delete('/posts', postController.remove)
+app.patch('/posts/:id', checkAuth, postController.update)
+app.delete('/posts/:id', checkAuth, postController.remove)
 
-// пишем и запускаем сервер командой "node <file.js>", либо через nodemon
+// пишем и запускаем сервер командой "node <file.js>", либо через "nodemon <file.js>"
 app.listen(3333, error => {
   if (error) {
     return console.log(error)
